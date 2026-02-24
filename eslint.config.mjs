0d1a116e51020fdb14a5d8abf9e426216ead724e
PunnyBunny <sunnycheungabc@gmail.com>
2026-02-24 14:52:24 +0000

    chore: slight update to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -39,6 +39,8 @@ export default defineConfig(
           },
         },
       ],
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     },
   },
   {
