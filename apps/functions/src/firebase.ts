fb391556f25733bb89e3dc8151883e375c801190
PunnyBunny <sunnycheungabc@gmail.com>
2026-04-24 17:46:50 +0100

    chore: credentials

--- apps/functions/src/firebase.ts
+++ apps/functions/src/firebase.ts
@@ -1,10 +1,6 @@
 import * as admin from "firebase-admin";
 
-// Initialize Firebase Admin once per instance
-import serviceAccountKey from "../firebase-service-account-key.json";
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey as admin.ServiceAccount),
   storageBucket: "quizzie-hku.firebasestorage.app",
 });
 
